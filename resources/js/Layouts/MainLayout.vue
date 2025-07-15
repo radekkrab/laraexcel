13cc90f30e3bb43687cf5b178e218514601f75ac
radekkrab <radxxx7@mail.ru>
2025-07-15 16:16:37 +0500

    fix: Inertia 1

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-4/6 mx-auto">
+        <navigation></navigation>
+
+        <slot/>
+    </div>
+</template>
+
+<script>
+import { Link } from  '@inertiajs/vue3'
+import Navigation from "@/Components/Navigation.vue";
+export default {
+    name: "MainLayout",
+
+    components: {
+        Link,
+        Navigation
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Project/Import.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        Import
+        <div class="flex justify-center">
+            <form class="flex">
+                <div class="mr-2">
+                    <input class="w-16 rounded-full" type="number" min="1" max="2" v-model="type">
+                </div>
+                <div>
+                <input @change="setExcel" type="file" ref="file" class="hidden">
+                <a @click.prevent="selectExcel" href="#"
+                   class="block rounded-full bg-green-600 w-32 text-center text-white p-2">Excel</a>
+                </div>
+            </form>
+            <div v-if="file" class="ml-3">
+                <a @click.prevent="importExcel" href="#"
+                   class="block rounded-full bg-sky-600 w-32 text-center text-white p-2">Import</a>
+            </div>
+        </div>
+        <div v-if="$page.props.flash.message" class="mt-4 text-green-600 text-center">
+            {{ $page.props.flash.message }}
+        </div>
+    </div>
+</template>
+
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "Import",
+    layout: MainLayout,
+
+    data() {
+        return {
+            file: null,
+            type: 1
+        }
+    },
+
+    methods: {
+        selectExcel() {
+            this.$refs.file.click()
+        },
+
+        setExcel(e) {
+            this.file = e.target.files[0]
+        },
+
+        importExcel() {
+            const formData = new FormData
+            formData.append('file', this.file)
+            formData.append('type', this.type)
+
+            this.$inertia.post('/projects/import', formData, {
+                onSuccess: () => {
+                    this.file = null
+                    this.$refs.file.value = null
+                }
+
+            })
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
