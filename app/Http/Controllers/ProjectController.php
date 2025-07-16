e564edecb5bbe87536eb5673228aaabb940bcc46
radekkrab <radxxx7@mail.ru>
2025-07-16 16:37:42 +0500

    feat: add ProjectController/ImportStore and Project/ImportStoreRequest

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Project\ImportStoreRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -14,5 +15,10 @@ class ProjectController extends Controller
 
     public function import(): Response {
         return Inertia::render('Project/Import');
+    }
+
+    public function importStore(ImportStoreRequest $request): Response {
+        $data = $request->validated();
+
     }
 }
